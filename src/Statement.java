196dcd9923e5a0eae798e807c04d9b2f25a42789
Elias Buenrostro <buenrostroelias@gmail.com>
2019-10-22 11:14:55 -0700

    DTO start

---
+++ src/Statement.java
@@ -0,0 +1,12 @@
+public class Statement {
+    int count;
+    String statement;
+    public Statement(int count, String statement){
+        this.count = count;
+        this.statement = statement;
+    }
+
+    public boolean racistOrNot(){
+
+    }
+}
