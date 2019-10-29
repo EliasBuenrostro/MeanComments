01d2d90d63a6882d50062e811a5e03cf237d1ee8
ninathma <nina.athma@gmail.com>
2019-10-29 16:48:24 -0700

    added in word bins

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isRacist(""));
+        String[] testsuite=Read.getTestSuite();
+        ArrayList<Boolean> results=new ArrayList<Boolean>();
+        for (int i = 0; i < testsuite.length; i++) {
+            Statement testStatement=new Statement(i, testsuite[i]);
+            results.add(isRacist(testStatement));
+        }
+
+        System.out.println(results);
     }
 
-    private static boolean isRacist(String s) {
-        Statement statement=new Statement(1, s);
+    private static boolean isRacist(Statement statement) {
         statement.getRawWords();
-        System.out.println("derogatory word= "+statement.containsDerogatoryWord());
-        System.out.println("race classification= "+statement.containsRaceClassification());
-        System.out.println("adjacence= "+statement.raceClassAdjacent());
+//        System.out.println("derogatory word= "+statement.containsDerogatoryWord());
+//        System.out.println("race classification= "+statement.containsRaceClassification());
+//        System.out.println("adjacence= "+statement.raceClassAdjacent());
         if (statement.containsDerogatoryWord()) {
             if (statement.containsRaceClassification()) {
                 if (statement.raceClassAdjacent()) {
