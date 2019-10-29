5924d3a03cf62667f83914b7287701d5df24f7a8
Elias Buenrostro <buenrostroelias@gmail.com>
2019-10-29 10:46:16 -0700

    "Main" first commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(isRacist(""));
+    }
+
+    private static boolean isRacist(String s) {
+        Statement statement=new Statement(1, s);
+        statement.getRawWords();
+        System.out.println("derogatory word= "+statement.containsDerogatoryWord());
+        System.out.println("race classification= "+statement.containsRaceClassification());
+        System.out.println("adjacence= "+statement.raceClassAdjacent());
+        if (statement.containsDerogatoryWord()) {
+            if (statement.containsRaceClassification()) {
+                if (statement.raceClassAdjacent()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
